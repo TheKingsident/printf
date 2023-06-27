4a73186a673254391f276ddb0255eae872b0adca
Kingsley Usa <kingsident@gmail.com>
2023-06-27 20:27:38 +0100

    Updated _printf.c file

--- _printf.c
+++ _printf.c
@@ -3,16 +3,15 @@
 /**
  * _printf - work like std printf function
  * @format: Format of string
- *
  * Return: number of characters printed
  */
 
 int _printf(const char *format, ...)
 {
 	int i;
-	/**char format_specifier; */
 	int character_count;
 	va_list function_arguments;
+
 	va_start(function_arguments, format);
 
 	character_count = 0;
@@ -24,73 +23,13 @@ int _printf(const char *format, ...)
 		if (format[i] != '%')
 		{
 			write(1, &format[i], 1); /** Writes a single character to stdout */
-			character_count++; } /** Increment the character count for printed character */
+			character_count++; }
 		else
 		{
 			/** Handle format specifiers */
 			i++; /** To get the next character after '%' */
 			character_count += handle_format_specifier(format[i], function_arguments);
 
-			/** format_specifier = format[i]; 
-
-			if (format_specifier == 'd' || format_specifier == 'i')
-			{
-				int number_argument;
-
-				number_argument = va_arg(function_arguments, int);
-				character_count += print_integer(number_argument);  }
-			
-			else if (format_specifier == 'u')
-			{
-				unsigned int unsigned_argument;
-
-				unsigned_argument = va_arg(function_arguments, unsigned int);
-				character_count += print_unsigned_int(unsigned_argument); }
-
-			else if (format_specifier == 'c')
-			{
-				char character_arguments;
-				character_arguments = (char)va_arg(function_arguments, int);
-				character_count += print_character(character_arguments); }
-	
-			else if (format_specifier == 's')
-			{
-				char* string_arguments;
-
-				string_arguments = va_arg(function_arguments, char*);
-				character_count += print_string(string_arguments); }
-			else if (format_specifier == 'x')
-			{
-				unsigned int hexadecimal_arguments;
-
-				hexadecimal_arguments = va_arg(function_arguments, unsigned int);
-				character_count += print_hexadecimal(hexadecimal_arguments); }
-			else if (format_specifier == 'X')
-			{
-				unsigned int uc_hexadecimal_arguments;
-
-				uc_hexadecimal_arguments = va_arg(function_arguments, unsigned int);
-				character_count += print_uc_hexadecimal(uc_hexadecimal_arguments); }
-			else if (format_specifier == 'o')
-			{
-				unsigned int octal_arguments;
-
-				octal_arguments = va_arg(function_arguments, unsigned int);
-				character_count += print_octal(octal_arguments); }
-			else if (format_specifier == 'p')
-			{
-				void* pointer_arguments;
-
-				pointer_arguments = va_arg(function_arguments, void*);
-				character_count += print_address(pointer_arguments); }
-			else if (format_specifier == '%')
-			{
-				character_count += print_percentage(); }
-			else
-			{
-				write(1, &format[i - 1], 1);
-				write(1, &format[i], 1);
-				character_count += 2; } */
 		}
 	}
 
@@ -103,6 +42,7 @@ int _printf(const char *format, ...)
 /**
  *  handle_format_specifier - Handle the various format cases
  *  @format_specifier: The format specifier
+ *  @args: Contains the arguments
  *  Return: The length of printed characters
  */
 int handle_format_specifier(char format_specifier, va_list args)
